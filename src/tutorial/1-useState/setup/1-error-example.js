05149816d7ea74c65707fe407b3dd62c50fe6e2d
Francis Maina <sebastianfrancix@gmail.com>
2022-12-26 00:19:28 +0300

    covered useState basics

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const ErrorExample = () => {
   let title = "Random Title";
+
   const handleClick = () => {
+    title = "New Title";
     console.log(title);
   };
 
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [title, setTitle] = useState("Random Title");
+
+  const handleClick = () => {
+    if (title === "Random Title") {
+      setTitle("New Title");
+    } else {
+      setTitle("Random Title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
