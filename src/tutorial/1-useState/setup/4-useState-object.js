6d15d0ef93c58a0c6d7d6d4ee7b1e6e28d75eda4
Francis Maina <sebastianfrancix@gmail.com>
2022-12-26 01:12:52 +0300

    covered useState objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Francis",
+    age: 22,
+    message: "Wanna be the best engineer possible!",
+  });
+  // console.log(person);
+  const changeAge = () => {
+    setPerson({ ...person, message: "2023 is my year of growth" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button type="button" className="btn" onClick={changeAge}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
