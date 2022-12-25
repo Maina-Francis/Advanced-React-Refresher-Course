ceb5757fe8f60a91b8e19f14e8670f3b80c73075
Francis Maina <sebastianfrancix@gmail.com>
2022-12-26 02:37:48 +0300

    covered functional update state

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevNum) => {
+        return prevNum + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Basic Counter</h2>
+        <h1>{value}</h1>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value - 1)}
+        >
+          Decrease
+        </button>
+        <button type="button" className="btn" onClick={reset}>
+          Reset
+        </button>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value + 1)}
+        >
+          increase
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button type="button" className="btn" onClick={complexIncrease}>
+          Complex Increase
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
