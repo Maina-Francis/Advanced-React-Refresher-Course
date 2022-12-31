5c1278e8de15475f05100db59a68563f29e5f2c2
Francis Maina <sebastianfrancix@gmail.com>
2022-12-31 12:30:55 +0300

    added a basic form input with type 'submit'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import ConditionalRendering from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import ControlledInputs from "./tutorial/4-forms/setup/1-controlled-inputs";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
-    <div className="container">
-      <ConditionalRendering />
-    </div>
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input type="text" id="email" name="email" />
+            <button type="submit">Add Person</button>
+          </div>
+        </form>
+      </article>
+    </>
   );
 }
 
