b6706c8d2a6f75fa55e2c070183163183cd41ce6
Francis Maina <sebastianfrancix@gmail.com>
2022-12-31 14:16:42 +0300

    added functionality to add items to our list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -9,11 +9,28 @@ const ControlledInputs = () => {
   // states
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName);
-    console.log(email);
+    // console.log(firstName, email);
+
+    if (firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName: firstName,
+        email: email,
+      };
+      // console.log(person);
+      setPeople((people) => {
+        return [...people, person];
+      });
+      // console.log(people);
+    } else {
+      console.log("False values passed");
+    }
+    setFirstName("");
+    setEmail("");
   };
   return (
     <>
@@ -42,6 +59,15 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">Add Person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
