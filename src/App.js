a25e58c8358d7b90b8612f67b7bc9e554a755b39
Francis Maina <sebastianfrancix@gmail.com>
2022-12-31 13:26:36 +0300

    added a basic form input with type 'submit' on ControlledInputs component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,10 @@ import React from "react";
 import ControlledInputs from "./tutorial/4-forms/setup/1-controlled-inputs";
 
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target);
-  };
   return (
-    <>
-      <article>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label htmlFor="firstName">Name : </label>
-            <input type="text" id="firstName" name="firstName" />
-          </div>
-
-          <div className="form-control">
-            <label htmlFor="email">Email : </label>
-            <input type="text" id="email" name="email" />
-            <button type="submit">Add Person</button>
-          </div>
-        </form>
-      </article>
-    </>
+    <div className="container">
+      <ControlledInputs />
+    </div>
   );
 }
 
