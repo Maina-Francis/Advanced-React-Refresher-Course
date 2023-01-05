3868ffa659fcd4fe42a365131cdbc07fc5b4c648
Francis Maina <sebastianfrancix@gmail.com>
2023-01-05 05:59:20 +0300

    covered useRef hook

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,35 @@
-import React, { useEffect, useRef } from 'react';
+import { render } from "@testing-library/react";
+import React, { useEffect, useRef } from "react";
 
-// preserves value
-// DOES NOT trigger re-render
-// target DOM nodes/elements
+// preserves the value
+// does not trigger a re-render
+// targets DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  // ref container
+  const inputElement = useRef(null);
+  const h2Element = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputElement.current.value);
+    console.log(h2Element.current.innerText);
+  };
+  // console.log(inputElement);
+
+  useEffect(() => {
+    inputElement.current.focus();
+  });
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" ref={inputElement} />
+        <button type="submit">Submit</button>
+      </form>
+      <h2 ref={h2Element}>Hello world</h2>
+    </>
+  );
 };
 
 export default UseRefBasics;
